785b9a702a1aad6881eac55e4d1bde7ba02e8357
huangyihang <huangyihang@gmail.com>
2023-01-17 10:04:00 +0800

    代码格式化

--- src/main/java/com/example/mall/MallServerApplication.java
+++ src/main/java/com/example/mall/MallServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MallServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MallServerApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(MallServerApplication.class, args);
+  }
 
 }
--- src/test/java/com/example/mall/MallServerApplicationTests.java
+++ src/test/java/com/example/mall/MallServerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MallServerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+  @Test
+  void contextLoads() {
+  }
 
 }
