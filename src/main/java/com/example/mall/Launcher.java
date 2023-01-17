b0de8bbdcd2303efcf40ea3e4e2bc31ccdc80e56
huangyihang <huangyihang@gmail.com>
2023-01-17 17:16:39 +0800

    引入轻量级 HTTP 客户端 OpenFeign

--- src/main/java/com/example/mall/Launcher.java
+++ src/main/java/com/example/mall/Launcher.java
@@ -3,10 +3,12 @@ package com.example.mall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class Launcher {
 
   public static void main(String[] args) {
